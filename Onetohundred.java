64021886ce8dca21b9884205fb87362640f15731
tejaswikoppula <85212514+tejaswikoppula@users.noreply.github.com>
2022-10-14 00:13:51 -0400

    Add files via upload
    Hello professor,
    Sorry for late submission,
    Thanks&regards,
    Tejaswi Koppula.

---
+++ Onetohundred.java
@@ -0,0 +1,17 @@
+package learning;
+
+public class Onetohundred {
+	public static void main(String args []) {
+		int i;
+		int num=100;
+		for(i=1;i<=num;i++) {
+			if(i%2==0&&i<=20) {
+				System.out.println(i);
+				
+			}
+			
+				
+			}
+		}
+		
+		}
